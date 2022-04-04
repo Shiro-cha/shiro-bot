09e6a6bd0588639e03041202076fb009a8e2de06
Shiro-cha <noum.rzdr@gmail.com>
2022-04-04 17:46:45 +0300

    with text de aona

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ function handleMessage(sender_psid, received_message) {
 		
 		// Create the payload for a basic text message
 		response = {
-			"text": `You sent the message: "${received_message.text}". Now send me an image!`
+			"De aona :D"
 		}
 	}  
 	
